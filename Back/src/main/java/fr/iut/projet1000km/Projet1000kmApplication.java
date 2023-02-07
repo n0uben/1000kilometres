449a8f5f03701d2582e041e09166689321b844c2
DrYruo <oury.jordan@hotmail.com>
2023-02-07 12:58:27 +0100

    Correction d'une mauvaise configuration du serveur spring

--- Back/src/main/java/fr/iut/projet1000km/Projet1000kmApplication.java
+++ Back/src/main/java/fr/iut/projet1000km/Projet1000kmApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Projet1000kmApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Projet1000kmApplication.class, args);
 	}
+
 }
