6d11407f8825d36b36118efe421cea52e300b99c
DrYruo <oury.jordan@hotmail.com>
2023-02-07 16:37:19 +0100

    config de hibernate

---
+++ Back/src/main/java/iut/fr/projet1000km/Projet1000kmApplication.java
@@ -0,0 +1,13 @@
+package iut.fr.projet1000km;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet1000kmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet1000kmApplication.class, args);
+	}
+
+}
