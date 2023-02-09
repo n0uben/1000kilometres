61dc29ca7803fa0c42c9470939e6a0312c29c2e2
DrYruo <oury.jordan@hotmail.com>
2023-02-09 17:07:23 +0100

    rearrrangement du model Utilisateur

--- Back/src/main/java/iut/fr/projet1000km/services/UtilisateurService.java
+++ Back/src/main/java/iut/fr/projet1000km/services/UtilisateurService.java
@@ -1,15 +1,44 @@
 package iut.fr.projet1000km.services;
 
+import iut.fr.projet1000km.models.Carte;
 import iut.fr.projet1000km.models.Utilisateur;
+import iut.fr.projet1000km.repository.UtilisateurRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface UtilisateurService {
-    Utilisateur creer(Utilisateur utilisateur);
+@Service
+public class UtilisateurService {
 
-    List<Utilisateur> lire();
+    private final UtilisateurRepository utilisateurRepository;
 
-    Utilisateur modifier(Long id, Utilisateur utilisateur);
+    public UtilisateurService(UtilisateurRepository utilisateurRepository) {
+        this.utilisateurRepository = utilisateurRepository;
+    }
 
-    String supprimer(Long id);
+    public Optional<Utilisateur> getOne(Long id) {
+        return utilisateurRepository.findById(id);
+    }
+
+    public List<Utilisateur> getAll() {
+        return utilisateurRepository.findAll();
+    }
+
+    public Utilisateur creer(Utilisateur utilisateur) {
+
+        Utilisateur utilisateurACreer = new Utilisateur(utilisateur.getIdUtilisateur(), utilisateur.getPseudo(), utilisateur.getMotDePasse(), utilisateur.getNbPartiesJouees(), utilisateur.getNbPartiesGagnees());
+
+        return utilisateurRepository.save(utilisateurACreer);
+    }
+
+
+    public Utilisateur modifier(Utilisateur utilisateur) {
+        Utilisateur utilisateurAModifier = new Utilisateur(utilisateur.getIdUtilisateur(), utilisateur.getPseudo(), utilisateur.getMotDePasse(), utilisateur.getNbPartiesJouees(), utilisateur.getNbPartiesGagnees());
+        return utilisateurRepository.saveAndFlush(utilisateurAModifier);
+    }
+
+    public void supprimer(Long id) {
+        utilisateurRepository.deleteById(id);
+    }
 }
