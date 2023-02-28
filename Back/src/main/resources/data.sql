5bea80017962cafa4322828a0d9a68cc8593f61f
n0uben <benjamin.noureux@gmail.com>
2023-02-28 15:08:22 +0100

    ajoute entrées dans data.sql

--- Back/src/main/resources/data.sql
+++ Back/src/main/resources/data.sql
@@ -1,1 +1,9 @@
 INSERT INTO `utilisateur` (`id_utilisateur`, `mot_de_passe`, `nb_parties_gagnees`, `nb_parties_jouees`, `pseudo`) VALUES (NULL, 'blabla', '0', '0', 'test');
+INSERT INTO `utilisateur` (`id_utilisateur`, `mot_de_passe`, `nb_parties_gagnees`, `nb_parties_jouees`, `pseudo`) VALUES
+                                                                                                                      (1, 'blabla', 0, 0, 'test'),
+                                                                                                                      (2, 'KatzBG', 7, 6, 'Moumi'),
+                                                                                                                      (3, 'superMDP', 0, 0, 'John Rambo'),
+                                                                                                                      (4, 'DoggyBG', 5, 5, 'Midoune');
+
+INSERT INTO `carte` (`id_carte`, `effet`, `km`, `nb_dispo`, `nom`) VALUES
+    (3, 'Boule de feu', 0, 8, 'Flammenboule');
