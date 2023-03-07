e037ff90d01ee6dab226bf4b6c826d3c0372b990
Tiger <cocogolf9@gmail.com>
2023-03-07 20:08:53 +0100

    - Ajout des photos - Changement de la page de jeu

--- Front/1000Km-front/src/components/GameBoard.vue
+++ Front/1000Km-front/src/components/GameBoard.vue
@@ -7,24 +7,24 @@ export default {
 <template>
   <div class="container" id="roads">
     <div class="row">
-      <img class="player" src="../assets/images/skyline_sprite.png"/>
+      <img class="player1" src="../assets/images/skyline_sprite.png"/>
       <div class="road">
       </div>
     </div>
     <div class="row ">
-      <img class="player" src="../assets/images/skyline_sprite.png"/>
-      <div class="road">
-
-      </div>
-    </div>
-    <div class="row ">
-      <img class="player" src="../assets/images/skyline_sprite.png"/>
+      <img class="player2" src="../assets/images/skyline_sprite.png"/>
       <div class="road">
 
       </div>
     </div>
     <div class="row ">
-      <img class="player" src="../assets/images/skyline_sprite.png"/>
+      <img class="player3" src="../assets/images/skyline_sprite.png"/>
+      <div class="road">
+
+      </div>
+    </div>
+    <div class="row ">
+      <img class="player4" src="../assets/images/skyline_sprite.png"/>
       <div class="road">
 
       </div>
@@ -36,7 +36,12 @@ export default {
 
       </div>
       <div class="col-6 offset-1" id="main">
-
+        <img class="carte" src="../assets/images/Cartes/100kmh.PNG"/>
+        <img class="carte" style="margin-left: 2%" src="../assets/images/Cartes/50kmh.PNG"/>
+        <img class="carte" style="margin-left: 2%" src="../assets/images/Cartes/citerne.PNG"/>
+        <img class="carte" style="margin-left: 2%" src="../assets/images/Cartes/25kmh.PNG"/>
+        <img class="carte" style="margin-left: 2%" src="../assets/images/Cartes/crevaison.PNG"/>
+        <img class="carte" style="margin-left: 2%" src="../assets/images/Cartes/200kmh.PNG"/>
       </div>
       <div class="col-2 offset-1" id="defosse">
 
@@ -49,9 +54,25 @@ export default {
 #roads{
   margin-top: 4%;
 }
-
-.player{
+.carte{
+  width: 15% ;
+  margin-top: 6%;
+}
+.player1{
   width: 7%;
+  margin-left: 4%;
+}
+.player2{
+  width: 7%;
+  margin-left: 94%;
+}
+.player3{
+  width: 7%;
+  margin-left: 50%;
+}
+.player4{
+  width: 7%;
+  margin-left: 10%;
 }
 
 .road{
@@ -76,6 +97,7 @@ export default {
 
 #main{
   background-color: grey;
+  height: 100%;
 }
 
 #defosse{
