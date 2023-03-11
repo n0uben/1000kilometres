3f639b3ce2a0a0f6799cae641e347011e70a6ae4
n0uben <benjamin.noureux@gmail.com>
2023-03-11 11:08:18 +0100

    reformate config securite

--- src/main/java/iut/fr/projet1000km/ConfigSecurite.java
+++ src/main/java/iut/fr/projet1000km/ConfigSecurite.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -46,10 +47,9 @@ public class ConfigSecurite {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests((auth) -> {
-                    auth.anyRequest().authenticated();
-                }).cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()
+                ).cors(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
