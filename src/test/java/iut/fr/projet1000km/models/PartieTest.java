dfb7bac8d0dfe3faddd92fb974c2518630ec7805
n0uben <benjamin.noureux@gmail.com>
2023-03-11 18:56:47 +0100

    ajoute tests partie et utilisateur

---
+++ src/test/java/iut/fr/projet1000km/models/PartieTest.java
@@ -0,0 +1,47 @@
+package iut.fr.projet1000km.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartieTest {
+
+    Partie partie = new Partie();
+
+    @Test
+    void setNombreJoueurs1() {
+        partie.setNombreJoueurs(1);
+        Assertions.assertEquals(Constants.MIN_NB_JOUEURS, partie.getNombreJoueurs());
+    }
+
+    @Test
+    void setNombreJoueurs2() {
+        partie.setNombreJoueurs(2);
+        Assertions.assertEquals(2, partie.getNombreJoueurs());
+    }
+
+    @Test
+    void setNombreJoueurs12() {
+        partie.setNombreJoueurs(12);
+        Assertions.assertEquals(Constants.MAX_NB_JOUEURS, partie.getNombreJoueurs());
+    }
+
+    @Test
+    void setDureeTour20() {
+        partie.setDureeTour(20);
+        Assertions.assertEquals(Constants.MIN_DUREE_TOUR, partie.getDureeTour());
+    }
+
+    @Test
+    void setDureeTour45() {
+        partie.setDureeTour(45);
+        Assertions.assertEquals(45, partie.getDureeTour());
+    }
+
+    @Test
+    void setDureeTour120() {
+        partie.setDureeTour(120);
+        Assertions.assertEquals(Constants.MAX_DUREE_TOUR, partie.getDureeTour());
+    }
+}
---
+++ src/test/java/iut/fr/projet1000km/models/UtilisateurTest.java
@@ -0,0 +1,61 @@
+package iut.fr.projet1000km.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UtilisateurTest {
+
+    Utilisateur utilisateur = new Utilisateur();
+
+    @Test
+    void setKmParcourusTestBelowMin() {
+        utilisateur.setKmParcourus(-200);
+        Assertions.assertEquals(0, utilisateur.getKmParcourus());
+    }
+
+    @Test
+    void setKmParcourusTest200() {
+        utilisateur.setKmParcourus(200);
+        Assertions.assertEquals(200, utilisateur.getKmParcourus());
+    }
+
+    @Test
+    void setKmParcourusTestAboveMax() {
+        utilisateur.setKmParcourus(1200);
+        Assertions.assertEquals(0, utilisateur.getKmParcourus());
+    }
+
+    @Test
+    void setNbPartiesJoueesBelowMin() {
+        utilisateur.setNbPartiesJouees(10);
+        utilisateur.setNbPartiesGagnees(8);
+        utilisateur.setNbPartiesJouees(5);
+        Assertions.assertEquals(10, utilisateur.getNbPartiesJouees());
+    }
+
+    @Test
+    void setNbPartiesJouees20() {
+        utilisateur.setNbPartiesJouees(20);
+        Assertions.assertEquals(20, utilisateur.getNbPartiesJouees());
+    }
+
+    @Test
+    void setNbPartiesGagneesTestBelowMin() {
+        utilisateur.setNbPartiesGagnees(-1);
+        Assertions.assertEquals(0, utilisateur.getNbPartiesGagnees());
+    }
+
+    @Test
+    void setNbPartiesGagneesTest10() {
+        utilisateur.setNbPartiesJouees(12);
+        utilisateur.setNbPartiesGagnees(10);
+        Assertions.assertEquals(10, utilisateur.getNbPartiesGagnees());
+    }
+
+    @Test
+    void setNbPartiesGagneesTestAboveMax() {
+        utilisateur.setNbPartiesJouees(5);
+        utilisateur.setNbPartiesGagnees(10);
+        Assertions.assertEquals(0, utilisateur.getNbPartiesGagnees());
+    }
+}
