969dc5a2fba7c2660785d1d0f772f9da5871e2c0
n0uben <benjamin.noureux@gmail.com>
2023-03-11 11:26:38 +0100

    test exclusion datasourceconfig pr sonar

--- src/main/java/iut/fr/projet1000km/Projet1000kmApplication.java
+++ src/main/java/iut/fr/projet1000km/Projet1000kmApplication.java
@@ -2,8 +2,9 @@ package iut.fr.projet1000km;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Projet1000kmApplication {
 
 	public static void main(String[] args) {
