24a952bd0cb39f19855f092acaceaf11ad4f460a
BENJAMIN NOUREUX <benjamin.noureux@atos.net>
2023-04-11 00:13:53 +0200

    oopsie

--- src/main/java/iut/fr/projet1000km/controllers/UtilisateurControleur.java
+++ src/main/java/iut/fr/projet1000km/controllers/UtilisateurControleur.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/utilisateur")
-@CrossOrigin("http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UtilisateurControleur {
 
     private final UtilisateurService utilisateurService;
