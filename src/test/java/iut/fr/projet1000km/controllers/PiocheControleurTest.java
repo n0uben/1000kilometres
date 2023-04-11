ad5d56a789dcb9b776dc72567a1f60fa72422e06
n0uben <benjamin.noureux@gmail.com>
2023-04-11 12:58:47 +0200

    ajoute tests getbyid pioche controleur

--- src/test/java/iut/fr/projet1000km/controllers/PiocheControleurTest.java
+++ src/test/java/iut/fr/projet1000km/controllers/PiocheControleurTest.java
@@ -14,8 +14,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class PiocheControleurTest {
@@ -50,5 +52,33 @@ public class PiocheControleurTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(expectedJson))
                 .andReturn();
+    }
+
+    @Test
+    void testGetByIdPiocheExists() throws Exception {
+        Pioche pioche = new Pioche();
+        pioche.setIdPioche(1L);
+
+        String expectedJson = "{\"idPioche\":1,\"partie\":null,\"cartes\":null}";
+
+        when(piocheRepository.findById(any())).thenReturn(Optional.of(pioche));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/pioche/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expectedJson))
+                .andReturn();
+
+        verify(piocheRepository, times(1)).findById(any());
+    }
+
+    @Test
+    void testGetByIdPiocheNotFound() throws Exception {
+        when(piocheRepository.findById(any())).thenReturn(Optional.empty());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/pioche/1"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn();
+
+        verify(piocheRepository, times(1)).findById(any());
     }
 }
